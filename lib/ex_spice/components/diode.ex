5fd515d6793a8b2ae7e3f09d4c4be1ab8310a01e
Paulo Valente <polvalente@gmail.com>
2021-03-15 01:08:43 -0300

    feat: add diode to_string

--- lib/ex_spice/components/diode.ex
+++ lib/ex_spice/components/diode.ex
@@ -10,4 +10,27 @@ defmodule ExSpice.Components.Diode do
   def format, do: @format
 
   defstruct [:name, :node_pos, :node_neg, :current, vbe: 0.7]
+
+  defimpl ExSpice.Component, for: __MODULE__ do
+    def to_string(
+          %{
+            name: name,
+            node_pos: node_pos,
+            node_neg: node_neg,
+            vbe: vbe
+          },
+          netlist
+        ) do
+      [
+        name,
+        " ",
+        ExSpice.Netlist.translate_node(netlist, node_pos),
+        " ",
+        ExSpice.Netlist.translate_node(netlist, node_neg),
+        " ",
+        ExSpice.PrettyPrint.to_string(vbe),
+        "V"
+      ]
+    end
+  end
 end
